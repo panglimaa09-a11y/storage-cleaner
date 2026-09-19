79f215e75d8ca979d46ad6832b7abe04ea013401
panglimaa09-a11y <panglimaa09@gmail.com>
2026-09-19 17:56:41 +0700

    fix: point Android Gradle project to android/app

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,7 @@
 pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
+
 rootProject.name = "StorageCleaner"
+
 include(":app")
+project(":app").projectDir = file("android/app")
