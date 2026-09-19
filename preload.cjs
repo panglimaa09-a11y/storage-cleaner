f2e90700a234f53263817adca7956d41043e93c6
panglimaa09-a11y <panglimaa09@gmail.com>
2026-09-19 16:11:08 +0700

    feat: expose MTP scanner to renderer

--- preload.cjs
+++ preload.cjs
@@ -3,6 +3,8 @@ contextBridge.exposeInMainWorld('storageDesktop',{
  startScan:()=>ipcRenderer.invoke('scan-start'),
  cancelScan:()=>ipcRenderer.invoke('scan-cancel'),
  trashFiles:p=>ipcRenderer.invoke('trash-files',p),
+ mtpList:()=>ipcRenderer.invoke('mtp-list'),
+ mtpScan:p=>ipcRenderer.invoke('mtp-scan',p),
  onProgress:cb=>ipcRenderer.on('scan-progress',(_,d)=>cb(d)),
  onError:cb=>ipcRenderer.on('scan-error',(_,d)=>cb(d))
 });
